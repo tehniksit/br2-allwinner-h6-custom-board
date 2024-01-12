eea22c1a3be1a4be9d05f3e01904d137e3d1d368
Vlad Zhdanov <tehniksit@gmail.com>
2024-01-13 01:16:52 +0300

    Move uboot-2022.04 to external tree

--- board/allwinner_h6_custom_board/override.mk
+++ board/allwinner_h6_custom_board/override.mk
@@ -1,1 +1,2 @@
+UBOOT_OVERRIDE_SRCDIR=/home/vlad/dev/br2_allwinner_h6_custom/src/$(BR2_TARGET_UBOOT_CUSTOM_VERSION_VALUE)
 LINUX_OVERRIDE_SRCDIR=/home/vlad/dev/br2_allwinner_h6_custom/src/$(BR2_LINUX_KERNEL_VERSION)
